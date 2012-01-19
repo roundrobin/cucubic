f01c64c0f80ea4ba490442a2213c2e418dbdfd4c
Nils <nils.schlomann@gmail.com>
2012-01-19 17:03:23 +0100

    clean up

--- static/js/cucubic/javascriptExtensions.js
+++ static/js/cucubic/javascriptExtensions.js
@@ -3,3 +3,16 @@
         this.length = from < 0 ? this.length + from : from;
         return this.push.apply(this, rest);
       };
+
+      Array.prototype.remove=function(s){
+        var i = this.indexOf(s);
+        if(i != -1) this.splice(i, 1);
+        }
+        Array.prototype.contains=function(obj) {
+            for (var i = 0; i < this.length; i++) {
+                if (this[i] === obj) {
+                    return true;
+                }
+            }
+            return false;
+        } 
